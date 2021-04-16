62208b0a5a703987ffbd12461d3d89bc50954db4
Guilherme Agostin <guilhermeagostin@gmail.com>
2021-04-16 01:49:30 -0300

    Commit to main

---
+++ APIFATECForms/Arquivo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace APIFATECForms
+{
+    class Arquivo
+    {
+        
+
+        public void CriarPasta(string Pasta)
+        {        
+            // seu eu criar um metodo que ja inicializa criando todas as pastas ficaria dificil pra conseguir extrair tudo
+            if (Directory.Exists(Pasta))
+            {
+            }
+            else
+            {
+                Directory.CreateDirectory(Pasta);
+                Console.WriteLine("\nFoi gerada a pasta {0} dentro do projeto - {1}.", Pasta, Directory.GetCreationTime(Pasta));
+            }
+        }
+
+        public void ExtrairArquivo(string zipPath, string extractPath)
+        {
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+
+        public void DeletarArquivos()
+        {
+            if (System.IO.File.Exists(@"C:\Users\Public\DeleteTest\test.txt"))
+            {
+                // Use a try block to catch IOExceptions, to
+                // handle the case of the file already being
+                // opened by another process.
+                try
+                {
+                    System.IO.File.Delete(@"C:\Users\Public\DeleteTest\test.txt");
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+        }
+
+        public void DeletarArquivos2(string pasta)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(pasta);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if(file.Extension == ".zip")
+                {
+                    file.Delete();
+                }
+                
+            }
+          
+        }
+
+
+       
+
+    }
+}
